d153a682c5106c07f98a9e496b8cd085b95cc84d
* <akhil.abhi.b@gmail>
2021-04-09 11:08:16 +0530

    added

---
+++ README.md
@@ -0,0 +1,23 @@
+#All Toll 
+
+##one click hacking tools
+
+
+* **open your termux**
+* git clone https://github.com/AkhilAbhi/alltool
+* cd alltool
+* bash start.sh
+*
+
+
+
+
+
+
+
+###prank your friends 
+
+
+*visit https://akhilabhi.github.io/alltoolweb/
+
+*unlocking key
--- r.sh
+++ r.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+
+red='\033[1;91m'
+green='\033[1;92m'
+blue='\033[1;34m'
+yellow='\033[1;33m'
+cyan='\033[1;96m'
+reset='\033[0m'
+
+
+
+clear
+
 cd /data/data/com.termux/files/usr/etc
 rm -rf bash.bashrc
 echo -e "PS1='\$' " > bash.bashrc
+printf "${green}"
+figlet unlock
+printf "${reset}"
